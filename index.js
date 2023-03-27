887d6c3c0df63fad3b5e846b2459db8a6998f71f
Zach Hanson <zachhansonitp@gmail.com>
2023-03-27 10:06:52 -0500

    started working on Javascript file

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const {generateSVG, makeshape} = require('./lib/makesvg');
+
+inquirer 
+  .prompt ( [
+
+{
+  type: "input",
+  name: "text",
+  message: "Please enter your logo text (3 characters only)"
+
+},
+{
+  type: "input",
+  name: "textColor",
+  messsage: "Please enter a color keyword or hexa decimal number for your text's color"
+
+},
+{
+type: 'list',
+name: 'logoshape',
+message: "Please select logo shape (press enter when you have made your selection)",
+choices: ['circle', 'square', 'triangle'],
+},
+{
+  type: "input",
+  name: "logocolor",
+  message: "Please select a color for your logo or enter a hyxadecimal number",
+}
+
+
+
+
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Example dog object made from scratch. Writning 100 of these from scratch would take a long time.
 // const dog = {
 //   name: 'Rex',
@@ -20,4 +108,14 @@ function Dog(name, age, breed) {
   
   // Calling dog's nap method
   dog.nap();
-  
+    
+├── examples/           // Example svg file(s) created with the app
+├── lib/                // Folder for classes or functions
+    ├── shapes.js       // Exports `Triangle`, `Circle`, and `Square` classes
+    ├── shapes.test.js  // Jest tests for shapes
+    └── more...         // Additional files and tests
+├── .gitignore          // Indicates which folders and files Git should ignore
+├── index.js            // Runs the application using imports from lib/
+├── package.json
+└── README.md           // App description, link to video, setup and usage instructions           
+```
